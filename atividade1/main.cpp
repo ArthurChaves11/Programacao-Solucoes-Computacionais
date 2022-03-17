2ae1297ea05c49d0944378882a7c0841083bb020
ArthurChaves11 <83314952+ArthurChaves11@users.noreply.github.com>
2022-03-17 15:09:26 -0300

    Add files via upload

---
+++ atividade1/main.cpp
@@ -0,0 +1,39 @@
+//Na venda da esquina, as maçãs custam R$ 0,30 cada se forem compradasmenos do que uma dúzia, e R$ 0,25 se forem compradas pelomenos doze.
+//Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+
+
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
+
+
+    int macas = 0;
+    float preco_macas = 0;
+    float valor_total = 0;
+
+
+    cout << "Maçãs compradas: ";
+        cin >> macas;
+
+
+    if(macas < 12)
+    {
+        preco_macas = 0.30;
+        valor_total = macas * preco_macas;
+
+        cout << "O valor total foi de: R$" << valor_total;
+    }
+
+    else {
+        preco_macas = 0.25;
+        valor_total = macas * preco_macas;
+
+        cout << "O valor total foi de R$" << valor_total;
+    }
+
+}
+
